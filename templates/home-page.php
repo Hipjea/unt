5495054c816de695f0b0dc9c414951b7d833f8a2
Florian <florian.bailly42@gmail.com>
2021-05-05 12:42:35 +0200

    Header page d'accueil - autres pages (h1, h2)

--- templates/home-page.php
+++ templates/home-page.php
@@ -22,18 +22,21 @@ global $themeOptions;
 $timberContext['homePageModel'] = $homePageService->getHomePageData();
 $timberContext['aLaUneModel'] = $aLaUneService->getALaUneList();
 $timberContext['partnersList'] = $partnerService->getPartnersList();
+$timberContext['isHomePage'] = true;
 
 $projects = Timber::get_post(47);
 $children = $projects->{'children'};
 
-foreach ($children as $child) {
-    if ($child->post_content != "") {
-        $top_image_id = $child->top_image;
-        $child->topImage = new \Timber\Image($top_image_id);
-        $timberContext['projects_children'][] = $child;
+if (count($children) > 0) {
+    foreach ($children as $child) {
+        if ($child->post_content != "") {
+            $top_image_id = $child->top_image;
+            $child->topImage = new \Timber\Image($top_image_id);
+            $timberContext['projects_children'][] = $child;
+        }
     }
+    $timberContext['projects_children'] = array_reverse($timberContext['projects_children']);
 }
-$timberContext['projects_children'] = array_reverse($timberContext['projects_children']);
 
 if ($themeOptions->isSolrEnabled()) {
 	$listeZoomSur = $solrService->getZoomSurResults($themeOptions->getZoomSolrRequest());
