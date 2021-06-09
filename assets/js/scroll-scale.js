5f844f723057087c5e9fe752b2c43a77678bf00b
Pierre <pierre.duverneix@gmail.com>
2021-06-09 17:41:34 +0200

    fix --title-maincolor & --title-seccolor

---
+++ assets/js/scroll-scale.js
@@ -0,0 +1,23 @@
+$(window).on('resize scroll', function() {
+    if ($('.scale-card').length > 0) {
+        $.fn.isInViewport = function() {
+            var elementTop = $('#partners-anchor').offset().top;
+            var elementBottom = elementTop + $(this).outerHeight();
+            var viewportTop = $(window).scrollTop();
+            var viewportBottom = viewportTop + $(window).height();
+            return elementBottom > viewportTop && elementTop < viewportBottom;
+        };
+
+        var cards = $('.scale-card');
+
+        if ($('#partners-anchor').isInViewport() && cards.length > 0) {
+            cards.each(function(index) {
+                $(this).addClass('inview');
+            });
+        } else {
+            cards.each(function(index) {
+                $(this).removeClass('inview');
+            });
+        }
+    }
+});
