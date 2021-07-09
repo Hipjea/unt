395b2fbe0ba59c83fef8e7a68533f7dd5892fb61
Pierre <pierre.duverneix@gmail.com>
2021-07-09 16:15:43 +0200

    allow svg in theme settings

--- functions.php
+++ functions.php
@@ -128,3 +128,9 @@ remove_action('wp_head', 'print_emoji_detection_script', 7);
 remove_action('wp_print_styles', 'print_emoji_styles');
 remove_action('admin_print_scripts', 'print_emoji_detection_script');
 remove_action('admin_print_styles', 'print_emoji_styles');
+// Allow SVG files
+function wpc_mime_types($mimes) {
+	$mimes['svg'] = 'image/svg+xml';
+	return $mimes;
+}
+add_filter('upload_mimes', 'wpc_mime_types');
